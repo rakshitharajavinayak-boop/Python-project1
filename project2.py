19560771fc26d75a64823c6226dc52873635e4ea
rakshitharajavinayak-boop <rakshitharajavinayak@gmail.com>
2025-12-07 17:07:38 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,23 @@
+num1 = 45
+num2 = 3
+ 
+print("Number 1",num1)
+print("Number 2", num2)
+print("addition :", num1 + num2)
+print("differance :", num1 - num2)
+print("product :" ,num1 * num2)
+print("Division:", num1 / num2) 
+print("Floor Division :" ,num1 // num2)
+print("Modulus operation :", num1 % num2) 
+print("square :" ,num2**2)
+print("Square root :", num1**0.5)
+
+
+print("Equal ?", num1==num2)
+print("num 1 greater?",num1>num2)
+print("number 2 greater?", num1<num2)
+print("not Equal /",num1!=num2)
+
+
+result = num1/2+num2**2+10
+print("result of given equation is :",result)
