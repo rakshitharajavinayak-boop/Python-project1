8bf91a843275aa214ed80182a5ea7bb38904094d
rakshitharajavinayak-boop <rakshitharajavinayak@gmail.com>
2026-02-01 15:51:25 +0530

    Add files via upload

---
+++ project10(guess the numder).py
@@ -0,0 +1,22 @@
+list_1=[59,38,94,85,42,15,67,98,29,35]
+print("this is the list:",list_1)
+x=int(input("guess the number choosen by the judge:" ))
+
+if x<35:
+    print("guess a greater number")
+elif x>35:
+    print("guess a smaller number")
+else :
+    print("wow! you guessed it right")
+x=int(input("guess the number choosen by the judge:" ))
+if x<35:
+    print("guess a greater number")
+elif x>35:
+    print("guess a smaller number")
+else :
+    print("wow! you guessed it right")
+x=int(input("guess the number choosen by the judge:" ))
+if x==35:
+    print("wow! you guessed it right")
+else:
+    print("better luck next time:(")
