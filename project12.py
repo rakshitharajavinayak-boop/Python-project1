e2d43bd1a290f29934b4d7d03a021bce01bf1c0b
rakshitharajavinayak-boop <rakshitharajavinayak@gmail.com>
2026-02-14 16:01:00 +0530

    Add files via upload

---
+++ project12.py
@@ -0,0 +1,14 @@
+class robot:
+    def __init__(self, name):
+        self.name = name
+    def intro(self):
+        print("Hello, I am", self.name,"the robot")
+robot1 = robot('Tom')
+robot1.intro()
+class robot:
+    def __init__(self, name):
+        self.name = name
+    def intro(self):
+        print("Hello, I am", self.name,"the robot")
+robot2 = robot('Jerry')
+robot2.intro()
