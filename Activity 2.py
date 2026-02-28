225414076e7f02072687750a6ae1e2f00d40f9b9
rakshitharajavinayak-boop <rakshitharajavinayak@gmail.com>
2026-02-28 17:25:29 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,8 @@
+file1 = open('Codingal.txt','r')
+file2 = open('operations on file part1\CodingalUpdated.txt','w')
+for line in file1.readlines():
+    if not (line.startswith('Coding')):
+        print(line)
+        file2.write(line)
+file1.close()
+file2.close()
---
+++ read operations part2.py
@@ -0,0 +1,13 @@
+file_read = open('C:\\Users\\rajav\\Downloads\\Codingal.txt','r')
+print("File in Read Mode -")
+print(file_read.read())
+file_read.close()
+file_write = open('C:\\Users\\rajav\\Downloads\\Codingal.txt','w')
+file_write.write("File in write mode....")
+file_write.write("Hi! I am Rakshitha.I am 13 yr. old")
+file_write.close()
+file_append = open('Codingal.txt','a')
+file_append.write("\n File in append mode .... ")
+file_append.write("Hi! I am penquin.I am 1yr.old")
+file_append.close()
+
